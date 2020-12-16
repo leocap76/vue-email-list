0ad91393b33d14b5a907fdf30c4edc25a90c5844
Leonardo Capogna <leonardocapogna@gmail.com>
2020-12-16 17:04:14 +0100

    error js22

--- js/script.js
+++ js/script.js
@@ -2,29 +2,31 @@
  // e stamparle a schermo,
 //  utilizzando Vue
 
-// var app = new Vue({
-//
-//    el:"#root",
-//
-//    data: {
-//       emailList: [],
-//       Emails: 10,
-//    },
-//
-//    methods: {
-//
-//     },
-// });
+var app = new Vue({
 
-for(i=0; i<10; i++) {
-    axios
-      .get('https://flynn.boolean.careers/exercises/api/random/mail')
-      .then(function (response) {
-        const result = response.data;
-          console.log(response.data);
-      }
-    )
-  }
+   el:"#root",
+
+   data: {
+      emailList: [],
+      Emails: 10,
+   },
+
+   mounted: function() {
+     for(i=0; i<10; i++) {
+         axios
+           .get('https://flynn.boolean.careers/exercises/api/random/mail')
+           .then(function (response) {
+             const result = response.data;
+               console.log(response.data);
+               // document.write(result);
+               emailList.push(result);
+           }
+         )
+       }
+    }
+});
+
+
 
       // VUE_____________________
       // axios
