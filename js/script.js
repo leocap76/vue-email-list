dbbb05747b28db01a8d2c3ab13b12bc3a2030d08
Leonardo Capogna <leonardocapogna@gmail.com>
2020-12-16 16:35:31 +0100

    console.log

--- js/script.js
+++ js/script.js
@@ -1,3 +1,48 @@
 // Esercizio: Generare 10 email
  // e stamparle a schermo,
 //  utilizzando Vue
+
+// var app = new Vue({
+//
+//    el:"#root",
+//
+//    data: {
+//       emailList: [],
+//       Emails: 10,
+//    },
+//
+//    methods: {
+//
+//     },
+// });
+
+for(i=0; i<10; i++) {
+    axios
+      .get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then(function (response) {
+        const result = response.data;
+          console.log(response.data);
+      }
+    )
+  }
+
+      // VUE_____________________
+      // axios
+      // .get('https://flynn.boolean.careers/exercises/api/random/mail')
+      // .then(function (response) {
+      // const result = response.data;
+
+
+
+     // jQUERY___________________
+// $.ajax(
+//     {
+//     url: "https://flynn.boolean.careers/exercises/api/random/mail",
+//     method: "GET",
+//     success: function (data, stato) {
+//     $("#risultati").html(data);
+//     },
+//     error: function (richiesta, stato, errori) {
+//     alert("E' avvenuto un errore. " + errore);
+//     }
+// });
